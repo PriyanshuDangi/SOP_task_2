c37924a967131335d716049b9da2beb6cc486397
PriyanshuDangi <priyanshudangipd@gmail.com>
2020-06-05 17:44:18 +0530

    added env variable

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/spidertask3', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log('connected to database')
+});
